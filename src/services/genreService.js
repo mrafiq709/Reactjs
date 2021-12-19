2ffa238511171d49b32176f23a49c22712c18993
mrafiq709 <mrafiq709@gmail.com>
2021-12-19 13:14:36 +0700

    add headers

--- src/services/genreService.js
+++ src/services/genreService.js
@@ -1,10 +1,11 @@
+import { getHeaders } from "../utils/heades";
 import http from "./httpService";
 
 export function getGenres() {
-  return http.get("https://rest-api.ml/api/genres");
+  return http.get("https://rest-api.ml/api/genres", getHeaders());
 }
 
 export function getGenre($id) {
   console.log($id);
-  return http.get("https://rest-api.ml/api/genres/" + $id);
+  return http.get("https://rest-api.ml/api/genres/" + $id, getHeaders());
 }
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,18 +1,19 @@
 import http from "./httpService";
 import { SERVER_REST_API } from "./../constants/apiConfig";
+import { getHeaders } from "../utils/heades";
 
 function movieUrl(id) {
   return `${SERVER_REST_API}/api/movies/${id}`;
 }
 
 export function getMovies() {
-  return http.get(`${SERVER_REST_API}/api/movies`);
+  return http.get(`${SERVER_REST_API}/api/movies`, getHeaders());
 }
 
 export function getMovie(id) {
-  return http.get(movieUrl(id));
+  return http.get(movieUrl(id), getHeaders());
 }
 
 export function saveMovie(movie) {
-  return http.post(`${SERVER_REST_API}/api/movies`, movie);
+  return http.post(`${SERVER_REST_API}/api/movies`, movie, getHeaders());
 }
